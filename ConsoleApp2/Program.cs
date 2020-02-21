0cf7deed7836d04228be6b7ce1f479c8dda80448
Никитенко Виктория Игоревна <@corp.itstep>
2020-02-21 16:05:03 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    interface ICar
+    {
+        int speed { get; set; }
+        int Move(int distance);
+    }
+    interface IPerson
+    {
+        string country { get; set; }
+        int eat(int toplivo);
+    }
+    class BMW : ICar, IPerson
+    {
+        public string country { get; set; }
+        public int speed { get; set; }
+        
+        public string Model { get; set; }
+        public void info()
+        {
+            Console.WriteLine(Model);
+        }
+        public int Move(int distance)
+        {
+            return distance / speed;
+        }
+
+        public int eat(int toplivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMW car = new BMW();
+            car.speed = 120;
+            car.Model = "i8";
+            car.country = "Китай";
+               
+            Console.WriteLine(car.Move(480));
+            Console.WriteLine(car.country);
+
+            Console.ReadLine();
+        }
+    }
+}
